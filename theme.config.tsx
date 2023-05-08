6440e3ba3bcf0b64fc88772c85a18b7cbe575317
liteli1987gmail <liteli1987@gmail.com>
2023-05-08 20:11:04 +0800

    增加入群二维码

--- theme.config.tsx
+++ theme.config.tsx
@@ -27,6 +27,7 @@ const config: DocsThemeConfig = {
         (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
    
       return <>
+        <title>LangChain中文网：500页超详细中文文档教程，助力LLM／chatGPT应用开发</title>
         <meta property="og:url" content={url} />
         <meta property="og:title" content={frontMatter.title || 'Nextra'} />
         <meta property="og:description" content={frontMatter.description || 'The next site builder'} />
@@ -46,6 +47,16 @@ const config: DocsThemeConfig = {
     link: 'https://www.Langchain.com.cn/about',
     icon: <svg width="24" height="24" viewBox="0 0 248 204"><path fill="currentColor" d="M512 0C229.232 0 0 229.232 0 512s229.232 512 512 512 512-229.232 512-512S794.768 0 512 0zM358.4 358.4c39.936-39.936 104.448-39.936 144.384 0l89.088 89.088c7.168 7.168 18.752 7.168 25.92 0l89.088-89.088c39.936-39.936 104.448-39.936 144.384 0 39.936 39.936 39.936 104.448 0 144.384l-89.088 89.088c-7.168 7.168-7.168 18.752 0 25.92l89.088 89.088c39.936 39.936 39.936 104.448 0 144.384-39.936 39.936-104.448 39.936-144.384 0l-89.088-89.088c-7.168-7.168-18.752-7.168-25.92 0l-89.088 89.088c-39.936 39.936-104.448 39.936-144.384 0-39.936-39.936-39.936-104.448 0-144.384l89.088-89.088c7.168-7.168 7.168-18.752 0-25.92l-89.088-89.088C318.464 398.848 318.464 398.848 358.4 358.4z"/></svg>,
   },  
+  toc: {
+    float: true,
+    extraContent:(
+      <div>
+        <center>入群学习</center>
+        <img src="http://tuchuang.zaibk.com/2023/05/08/9810fccea7dda.png" alt="A description of your image" />
+      </div>
+    ),
+
+  },
   
   footer: {
     text: <span>MIT {new Date().getFullYear()} © <a href="https://www.Langchain.com.cn" target="_blank">Langchain中文网</a>. 跟着langchain学AI应用开发</span>,
